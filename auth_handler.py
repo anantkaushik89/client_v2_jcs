57047cea868c75ec4bc9433dd0e33eefd570648a
anantkaushik89 <anantkaushik.nsit@gmail.com>
2016-01-11 05:44:22 +0000

    initial version of client

---
+++ auth_handler.py
@@ -0,0 +1,90 @@
+import base64
+import copy
+import datetime
+import time
+import hmac
+import os
+import posixpath
+import hashlib
+import six
+from six.moves import urllib
+
+class HTTPRequest(object):
+
+    def __init__(self, method, protocol, host, port, path, auth_path,
+                 params, headers, body):
+        self.method = method
+        self.protocol = protocol
+        self.host = host
+        self.port = port
+        self.path = path
+        if auth_path is None:
+            auth_path = path
+        self.auth_path = auth_path
+        self.params = params
+        # chunked Transfer-Encoding should act only on PUT request.
+        self.headers = headers
+        self.body = body
+
+    def __str__(self):
+        return (('method:(%s) protocol:(%s) host(%s) port(%s) path(%s) auth_path(%s)'
+                 ' params(%s) headers(%s) body(%s)') % (self.method,
+                 self.protocol, self.host, self.port, self.path, self.auth_path, self.params,
+                 self.headers, self.body))
+
+
+class V2Handler(object):
+    
+    def __init__(self, host, service_name=None, region_name=None):
+        # You can set the service_name and region_name to override the
+        # values which would otherwise come from the endpoint, e.g.
+        # <service>.<region>.amazonaws.com.
+        self.host = host
+        self.service_name = service_name
+        self.region_name = region_name
+        self.access_key = os.environ.get('JCS_ACCESS_KEY')
+        self.secret_key = os.environ.get('JCS_SECRET_KEY')
+
+    def add_params(self, req):
+        req.params['AWSAccessKeyId'] = self.access_key
+        req.params['SignatureVersion'] = '2'
+        req.params['SignatureMethod'] = 'HmacSHA256'
+        req.params['Timestamp'] = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
+
+    @staticmethod
+    def _get_utf8_value(value):
+        """Get the UTF8-encoded version of a value."""
+        if not isinstance(value, (six.binary_type, six.text_type)):
+            value = str(value)
+        if isinstance(value, six.text_type):
+            return value.encode('utf-8')
+        else:
+            return value
+
+    def sort_params(self, params):
+        keys = params.keys()
+        keys.sort()
+        pairs = []
+        for key in keys:
+            val = V2Handler._get_utf8_value(params[key])
+            val = urllib.parse.quote(val, safe='-_~')
+            pairs.append(urllib.parse.quote(key, safe='') + '=' + val)
+        qs = '&'.join(pairs)
+        return qs
+
+    def string_to_sign(self, req):
+        ss = req.method + '\n' + req.host + ':' + req.port + '\n'
+        ss += req.path + '\n'
+        self.add_params(req)
+        qs = self.sort_params(req.params)
+        ss += qs
+        return ss
+
+    def add_auth(self, req):
+        hmac_256 = hmac.new(self.secret_key, digestmod=hashlib.sha256)
+        canonical_string = self.string_to_sign(req)
+        hmac_256.update(canonical_string.encode('utf-8'))
+        b64 = base64.b64encode(hmac_256.digest()).decode('utf-8')
+        req.params['Signature'] = b64
+        return req
+
---
+++ create_request.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import os
+import sys
+import datetime
+import auth_handler
+import httplib
+params, headers = {}, {}
+method = 'GET'
+path, port, auth_path = '', '', ''
+body, host, protocol = '', '', ''
+
+def add_params(string):
+    length = len(string)
+    parts = string.split('&')
+    for p in parts:
+        (key, val) = p.split('=')
+        params[key] = val
+
+def requestify(request):
+    [initial, rest] = request.split('?')
+    parts = initial.split('/')
+    protocol, host_port = parts[0][ : -1], parts[2]
+    path = '/' + parts[3] + '/' + parts[4] + '/'
+    auth_path = path
+    [host, port] = host_port.split(':')
+    add_params(rest)
+    headers['User-Agent'] = 'curl/7.35.0'
+    headers['Content-Type'] = 'application/json'
+    reqObj = auth_handler.HTTPRequest(method, protocol, host, port, path, auth_path,
+                         params, headers, body)
+    authHandlerObj = auth_handler.V2Handler(host)
+    reqObj = authHandlerObj.add_auth(reqObj)
+    request_string = 'curl -X ' + reqObj.method
+    request_string += ' -H \"Accept-Encoding: identity\"'
+    request_string += ' -H \"User-Agent: ' + headers['User-Agent'] + '\"'
+    request_string += ' -H \"Content-Type: ' + headers['Content-Type'] + '\"'
+    request_string += ' \"' + initial + '?'
+    for keys in reqObj.params:
+        request_string += keys + '=' + reqObj.params[keys] + '&'
+    request_string = request_string[ : -1]
+    request_string += '\"'
+    return request_string
+
+def main():
+    req = sys.argv[1]
+    chngd_req = requestify(req)
+    print chngd_req
+    #os.system(chngd_req)
+
+main()
